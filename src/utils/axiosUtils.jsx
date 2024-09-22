eddde47b59008f7e160016010500c552fdd44502
Rachit Patel <rachitp509@gmail.com>
2024-09-22 18:28:05 +0530

    changes

--- src/utils/axiosUtils.jsx
+++ src/utils/axiosUtils.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
-  baseURL:"https://muta-engine.adaptable.app"
+  // baseURL:"https://muta-engine.adaptable.app"
+  baseURL: "https://muta-back.onrender.com",
 });
 
 export default axiosInstance;
