b7ec6b8444456cdb255575ecfb5d898c5b880937
Rachit Patel <80671047+ra463@users.noreply.github.com>
2024-09-22 18:39:33 +0530

    Update axiosUtils.jsx

--- src/utils/axiosUtils.jsx
+++ src/utils/axiosUtils.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
-  // baseURL:"https://muta-engine.adaptable.app"
-  baseURL: "https://muta-back.onrender.com",
+  baseURL:"https://muta-engine.adaptable.app"
 });
 
 export default axiosInstance;
