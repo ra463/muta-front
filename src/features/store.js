1b6ff944d8167b946d284cbff689d399df4f041b
Rachit Patel <80671047+ra463@users.noreply.github.com>
2024-09-22 18:41:00 +0530

    Update store.js

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 
-export const server = "https://muta-back.onrender.com/api/order";
+export const server = "https://muta-engine.adaptable.app/api/order";
 
 export default configureStore({
   reducer: {
