ab6ec540ea44959a48476e263812e6a04bfc0026
Rachit Patel <rachitp509@gmail.com>
2024-09-22 17:52:22 +0530

    changes

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 
-export const server = "https://muta-front.vercel.app/api/order";
+export const server = "https://muta-engine.adaptable.app/api/order";
 
 export default configureStore({
   reducer: {
